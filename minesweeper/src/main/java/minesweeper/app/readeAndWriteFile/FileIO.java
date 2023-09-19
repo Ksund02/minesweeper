ac72399df7c3f90ba1da0e10d37074f7315ea10c
oskarew <oskarew@stud.ntnu.no>
2023-09-19 10:14:05 +0200

    Moved filerelated to own folder

---
+++ minesweeper/src/main/java/minesweeper/app/readeAndWriteFile/FileIO.java
@@ -0,0 +1,43 @@
+package minesweeper.app.readeAndWriteFile;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import minesweeper.app.UserScore;
+
+public class FileIO {
+    private static final String filePath = System.getProperty("user.dir")+"/src/main/resources/minesweeper/highscore.csv";
+
+    public List<UserScore> readFromFile() {
+        List<UserScore> userScores = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            reader.readLine();
+            String line = reader.readLine();
+            while (line != null) {
+                String[] lineArray = line.split(",");
+                String name = lineArray[0];
+                int score = Integer.parseInt(lineArray[1]);
+                String date = lineArray[2];
+                userScores.add(new UserScore(name, score, date));
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return userScores;
+    }
+    
+    public static void main(String[] args) {
+        FileIO fileReader = new FileIO();
+        List<UserScore> userScores = fileReader.readFromFile();
+        for (UserScore userScore : userScores) {
+            System.out.println(userScore);
+        }
+    
+    }
+}
