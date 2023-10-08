d76aacbd58b0c54497ecb6bb302dc403330c6124
Christian Fredrik <cjohnsen41@gmail.com>
2023-10-08 23:51:57 +0200

    Refactored HighscoreFileManager and created tests. Test coverage is now at 97%

--- minesweeper/storage/src/main/java/storage/HighscoreFileManager.java
+++ minesweeper/storage/src/main/java/storage/HighscoreFileManager.java
@@ -13,22 +13,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class HighscoreFileManager {
 
     private static final File highscoreFile = new File("./../appdata/highscore.json");
-    private final File file;
 
-    public HighscoreFileManager() {
-        file = highscoreFile;
-    }
+    /**
+     * Private constructor to prevent instantiation, and to make jacoco not complain.
+     */
+    private HighscoreFileManager() {}
 
-    public HighscoreFileManager(String filePath) {
-        this.file = new File(filePath);
-    }
-
+    /**
+     * @return The highscore file.
+     */
     public static File getFile() {
         return highscoreFile;
-    }
-
-    public File getCustomFile() {
-        return file;
     }
 
     /**
@@ -37,27 +32,19 @@ public class HighscoreFileManager {
      * @param userScore The score which the player has achieved.
      */
     public static void writeToHighscore(UserScore userScore, File file) {
-
-        List<UserScore> userScores = null;
-        // Read in the old user scores
-        try {
-            userScores = readFromHighscore(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if (userScores == null) {
-            userScores = new ArrayList<>();
-        }
-
-        // Add the new user score
+        List<UserScore> userScores = readFromHighscore(file);
         userScores.add(userScore);
-
-        // Sort the userScores by score, lower score is better.
-        userScores.sort((a, b) -> a.getScore() - b.getScore());
-
-        // Write the scores to json-file
+        sortUserScores(userScores);
         writeToFile(userScores, file);
+    }
+
+    /**
+     * Sorts a list of UserScores by score, lower scores first.
+     * 
+     * @param userScores The list of UserScores which is to be sorted.
+     */
+    private static void sortUserScores(List<UserScore> userScores) {
+        userScores.sort((a, b) -> a.getScore() - b.getScore());
     }
 
     /**
@@ -66,60 +53,38 @@ public class HighscoreFileManager {
      * @param userScores The scores which are to be written to the highscore file.
      */
     private static void writeToFile(List<UserScore> userScores, File file) {
-
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, userScores);
         } catch (IOException e) {
+            // It is actually very unlikely that something will go wrong here.
+            // Since if you specify a file which does not exist, it will be created.
             e.printStackTrace();
         }
     }
 
     /**
-     * Reads from the highscore file and returns a list of UserScore objects
+     * Reads from the highscore file and returns a list of UserScore objects.
+     * If the file cannot be read, an empty list will be returned.
      * 
+     * @param file The file which is to be read.
      * @return List<UserScore>: A list containing all saved UserScores.
      */
-    public static List<UserScore> readFromHighscore(File file) throws IOException {
-        InputStream is = new FileInputStream(file);
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            return objectMapper.readValue(is, new TypeReference<List<UserScore>>() {
+    public static List<UserScore> readFromHighscore(File file) {
+        try (InputStream is = new FileInputStream(file)) {
+            ObjectMapper jsonReader = new ObjectMapper();
+            return jsonReader.readValue(is, new TypeReference<List<UserScore>>() {
             });
         } catch (IOException e) {
-            throw new RuntimeException("Failed to read highscore-file", e);
+            e.printStackTrace();
+            return new ArrayList<UserScore>();
         }
     }
 
     /**
-     * Removes all data from the highscore file
+     * Removes all data from the highscore file.
      */
-    public static void clearHighscore(File file) {
+    static void clearHighscore(File file) {
         writeToFile(new ArrayList<UserScore>(), file);
-
-    }
-
-    // Simple testing
-    public static void main(String[] args) {
-        UserScore alice = new UserScore("Alice", 200, "2023-01-01");
-        UserScore bob = new UserScore("Bob", 100, "2023-01-01");
-        UserScore charlie = new UserScore("Charlie", 300, "2023-01-01");
-        UserScore dave = new UserScore("Dave", 150, "2023-01-01");
-        UserScore eve = new UserScore("Eve", 250, "2023-01-01");
-        List<UserScore> userScores = new ArrayList<>();
-        userScores.add(alice);
-        userScores.add(bob);
-        userScores.add(charlie);
-        userScores.add(dave);
-        userScores.add(eve);
-        writeToHighscore(alice, highscoreFile);
-        writeToHighscore(bob, highscoreFile);
-        writeToHighscore(charlie, highscoreFile);
-        writeToHighscore(dave, highscoreFile);
-        writeToHighscore(eve, highscoreFile);
-
-        // clearHighscore(HighscoreFileManager.highscoreFile);
-        // System.out.println(readFromHighscore());
-
     }
 }
--- minesweeper/storage/src/test/java/storage/FileManagerTest.java
+++ minesweeper/storage/src/test/java/storage/FileManagerTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.PrintStream;
+import java.io.OutputStream;
 import java.util.List;
 
 public class FileManagerTest {
@@ -21,9 +23,32 @@ public class FileManagerTest {
     }
 
     @Test
-    public void writeToHighscore() throws Exception {
-        UserScore pedor = new UserScore("Pedor", 50, "2023-10-04");
+    public void checkFile() {
+        File expectedFile = new File("./../appdata/highscore.json");
+        File actualFile = HighscoreFileManager.getFile();
+        assertEquals(expectedFile, actualFile, "File is not correct");
+    }
+
+    @Test
+    public void clearHighscore() throws Exception {
+        UserScore bert = new UserScore("Bert", 100, "2020-10-04");
+        UserScore bernard = new UserScore("Bernard", 30, "2021-09-08");
+        UserScore alfred = new UserScore("Alfred", 50, "2021-09-15");
+        HighscoreFileManager.writeToHighscore(bert, testFile);
+        HighscoreFileManager.writeToHighscore(bernard, testFile);
+        HighscoreFileManager.writeToHighscore(alfred, testFile);
+
         HighscoreFileManager.clearHighscore(testFile);
+        ObjectMapper jsonReader = new ObjectMapper();
+        List<UserScore> userScores = jsonReader.readValue(testFile, new TypeReference<List<UserScore>>() {
+        });
+
+        assertEquals(0, userScores.size(), "File is not empty after clearing: " + userScores.toString());
+    }
+
+    @Test
+    public void writeToEmptyHighscore() throws Exception {
+        UserScore pedor = new UserScore("Pedor", 50, "2023-10-04");
         HighscoreFileManager.writeToHighscore(pedor, testFile);
         ObjectMapper jsonReader = new ObjectMapper();
         List<UserScore> userScores = jsonReader.readValue(testFile, new TypeReference<List<UserScore>>() {
@@ -36,8 +61,88 @@ public class FileManagerTest {
     }
 
     @Test
-    public void klubb() {
+    public void testHighscoreSorting() throws Exception {
+        UserScore alfa = new UserScore("Alfa", 100, "1908-01-04");
+        UserScore beta = new UserScore("Beta", 30, "1945-05-08");
+        UserScore gamma = new UserScore("Gamma", 50, "1940-04-09");
+        UserScore zeta = new UserScore("Zeta", 480, "2021-09-15");
+        HighscoreFileManager.writeToHighscore(alfa, testFile);
+        HighscoreFileManager.writeToHighscore(beta, testFile);
+        HighscoreFileManager.writeToHighscore(gamma, testFile);
+        HighscoreFileManager.writeToHighscore(zeta, testFile);
 
+        ObjectMapper jsonReader = new ObjectMapper();
+        List<UserScore> userScores = jsonReader.readValue(testFile, new TypeReference<List<UserScore>>() {
+        });
+
+        assertEquals(4, userScores.size(), "Size is not correct: " + userScores.toString());
+
+        assertEquals("Beta", userScores.get(0).getName(), "Name is not correct");
+        assertEquals(30, userScores.get(0).getScore(), "Score is not correct");
+        assertEquals("1945-05-08", userScores.get(0).getDate(), "Date is not correct");
+
+        assertEquals("Gamma", userScores.get(1).getName(), "Name is not correct");
+        assertEquals(50, userScores.get(1).getScore(), "Score is not correct");
+        assertEquals("1940-04-09", userScores.get(1).getDate(), "Date is not correct");
+
+        assertEquals("Alfa", userScores.get(2).getName(), "Name is not correct");
+        assertEquals(100, userScores.get(2).getScore(), "Score is not correct");
+        assertEquals("1908-01-04", userScores.get(2).getDate(), "Date is not correct");
+
+        assertEquals("Zeta", userScores.get(3).getName(), "Name is not correct");
+        assertEquals(480, userScores.get(3).getScore(), "Score is not correct");
+        assertEquals("2021-09-15", userScores.get(3).getDate(), "Date is not correct");
     }
 
+    /**
+     * If we have come to this test, then we know that writing to highscore works,
+     * therefore we can use HighscoreFileManager.writeToFile here..
+     * 
+     * @throws Exception If the file cannot be read.
+     */
+    @Test
+    public void readFromHighscore() throws Exception {
+        UserScore bert = new UserScore("Bert", 100, "2020-10-04");
+        UserScore bernard = new UserScore("Bernard", 200, "2021-09-08");
+        UserScore alfred = new UserScore("Alfred", 300, "2021-09-15");
+        HighscoreFileManager.writeToHighscore(bert, testFile);
+        HighscoreFileManager.writeToHighscore(bernard, testFile);
+        HighscoreFileManager.writeToHighscore(alfred, testFile);
+
+        List<UserScore> userScores = HighscoreFileManager.readFromHighscore(testFile);
+
+        assertEquals(3, userScores.size(), userScores.toString());
+
+        assertEquals("Bert", userScores.get(0).getName(), "Name is not correct");
+        assertEquals(100, userScores.get(0).getScore(), "Score is not correct");
+        assertEquals("2020-10-04", userScores.get(0).getDate(), "Date is not correct");
+
+        assertEquals("Bernard", userScores.get(1).getName(), "Name is not correct");
+        assertEquals(200, userScores.get(1).getScore(), "Score is not correct");
+        assertEquals("2021-09-08", userScores.get(1).getDate(), "Date is not correct");
+
+        assertEquals("Alfred", userScores.get(2).getName(), "Name is not correct");
+        assertEquals(300, userScores.get(2).getScore(), "Score is not correct");
+        assertEquals("2021-09-15", userScores.get(2).getDate(), "Date is not correct");
+    }
+
+    @Test
+    public void readFromNonExistingFile() {
+
+        // Setting up a new error stream, such that printstacktrace does not go off when
+        // running mvn test.
+        PrintStream originalErrorStream = System.err;
+        System.setErr(new PrintStream(new OutputStream() {
+            public void write(int b) { // Don't write anything.
+            }
+        }));
+
+        List<UserScore> userScores = HighscoreFileManager
+                .readFromHighscore(new File("src/test/resources/nonExistingHighscore.json"));
+        assertEquals(0, userScores.size(),
+                "File is somehow not empty, even though you have read from a file which does not exist.");
+
+        // Resetting the error stream.
+        System.setErr(originalErrorStream);
+    }
 }
