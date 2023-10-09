f358bfcc4bdb4218ff9a37c93af5c71339516ed5
Christian Fredrik <cjohnsen41@gmail.com>
2023-10-09 04:22:33 +0200

    Removed some unused imports.

--- minesweeper/core/src/test/java/core/GameBoardTest.java
+++ minesweeper/core/src/test/java/core/GameBoardTest.java
@@ -2,10 +2,8 @@ package core;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import core.GameBoardTestUtils;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
