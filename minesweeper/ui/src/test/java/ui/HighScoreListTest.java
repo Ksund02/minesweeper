26143206c6c5fb70b6a74004b1a9712992f390e2
oskarew <oskarew@stud.ntnu.no>
2023-10-11 20:23:52 +0200

    Minor refactory

--- minesweeper/ui/src/test/java/ui/HighScoreListTest.java
+++ minesweeper/ui/src/test/java/ui/HighScoreListTest.java
@@ -1,6 +1,5 @@
 package ui;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import java.util.List;
@@ -16,8 +15,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import storage.HighscoreFileManager;
 import storage.UserScore;
-import ui.HighscoreListController;
-
 
 public class HighScoreListTest extends ApplicationTest {
 
@@ -26,7 +23,6 @@ public class HighScoreListTest extends ApplicationTest {
      * 
      * 
      */
-    private HighscoreListController controller;
     private Parent root;
     private FxRobot robot;
 
@@ -34,23 +30,23 @@ public class HighScoreListTest extends ApplicationTest {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/ui/HighscoreList.fxml"));
         root = fxmlLoader.load();
-        controller = fxmlLoader.getController();
         stage.setScene(new Scene(root));
         stage.show();
         robot = new FxRobot();
     }
-
 
     @Test
     public void right_order() {
         List<UserScore> userScores = null;
         userScores = HighscoreFileManager.readFromHighscore(HighscoreFileManager.getFile());
         userScores.sort((a, b) -> a.getScore() - b.getScore());
-        for (int i=1;i<Math.min(11,userScores.size());i++) {
-            Assertions.assertEquals(""+userScores.get(i-1).getScore(), robot.lookup("#score"+i).queryLabeled().getText());
-            Assertions.assertEquals(userScores.get(i-1).getName(), robot.lookup("#name"+i).queryLabeled().getText());
-            Assertions.assertEquals(userScores.get(i-1).getDate(), robot.lookup("#date"+i).queryLabeled().getText());
+        for (int i = 1; i < Math.min(11, userScores.size()); i++) {
+            Assertions.assertEquals("" + userScores.get(i - 1).getScore(),
+                    robot.lookup("#score" + i).queryLabeled().getText());
+            Assertions.assertEquals(userScores.get(i - 1).getName(),
+                    robot.lookup("#name" + i).queryLabeled().getText());
+            Assertions.assertEquals(userScores.get(i - 1).getDate(),
+                    robot.lookup("#date" + i).queryLabeled().getText());
         }
-    }  
+    }
 }
-
