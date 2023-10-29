dabb8aa91341305d0e679738ffdd85981c5abecc
Christian Fredrik <cjohnsen41@gmail.com>
2023-10-30 00:01:58 +0100

    Ran checkstyle on HigscoreRestController, nothing added.

--- minesweeper/storage/src/main/java/springBoot/HighscoreRestController.java
+++ minesweeper/storage/src/main/java/springBoot/HighscoreRestController.java
@@ -2,7 +2,6 @@ package springBoot;
 
 import java.util.List;
 import storage.UserScore;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +14,16 @@ public class HighscoreRestController {
 
     private final HighscoreService highscoreService;
 
-
     /**
-     * Constructor for HighscoreRestController, which is used to handle HTTP requests.
-     * The great thing about Spring is that it automatically creates an instance of HighscoreService.
-     * This means that we don't have to create an instance of HighscoreService ourselves.
-     * @param highscoreService This is the class which contains the methods for handling HTTP requests.
+     * Constructor for HighscoreRestController, which is used to handle HTTP
+     * requests.
+     * The great thing about Spring is that it automatically creates an instance of
+     * HighscoreService.
+     * This means that we don't have to create an instance of HighscoreService
+     * ourselves.
+     * 
+     * @param highscoreService This is the class which contains the methods for
+     *                         handling HTTP requests.
      */
     @Autowired
     public HighscoreRestController(HighscoreService highscoreService) {
@@ -29,6 +32,7 @@ public class HighscoreRestController {
 
     /**
      * This method is used to handle GET requests to the /highscores endpoint.
+     * 
      * @return A list of UserScore objects.
      */
     @GetMapping("/highscores")
@@ -38,9 +42,11 @@ public class HighscoreRestController {
 
     /**
      * This method is used to handle POST requests to the /highscores endpoint.
-     * Spring Boot is super smart, and automatically converts the JSON string 
+     * Spring Boot is super smart, and automatically converts the JSON string
      * (which is sent in the request body) to a UserScore object.
-     * @param userScore This is the UserScore object which is written to the highscore file.
+     * 
+     * @param userScore This is the UserScore object which is written to the
+     *                  highscore file.
      */
     @PostMapping("/highscores")
     public void addHighscore(@RequestBody UserScore userScore) {
