90e4fefcca8231a48c33f9021a9a151fa32eabc3
Christian Fredrik <cjohnsen41@gmail.com>
2023-11-01 02:29:52 +0100

    New test: Test the highscoreService class.

---
+++ minesweeper/storage/src/test/java/storage/HighscoreServiceTest.java
@@ -0,0 +1,91 @@
+package storage;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+import springBoot.HighscoreService;
+import springBoot.SpringApp;
+
+/**
+ * This class is very similar to the HighscoreRestControllerTest class.
+ * The difference is that this class tests the HighscoreService class, which is
+ * in contrast to the controller test, where a mock version of HighscoreService
+ * is used.
+ * 
+ * So you can look at this class as an extension of the
+ * HighscoreRestControllerTest-class.
+ * It is possible that the ControllerTest-class passes all the tests, but the
+ * ServiceTest-class fails. This is because the ServiceTest-class tests the
+ * actual implementation of the service-methods, while the ControllerTest-class
+ * tests
+ * just tests that the HTTP requests are handled correctly.
+ */
+@SpringBootTest(classes = SpringApp.class)
+@AutoConfigureMockMvc
+public class HighscoreServiceTest {
+    @Autowired
+    private MockMvc mockMvc; // This guy is used to simulate HTTP interactions. It's a mock, so it's not a
+                             // real server.
+
+    @Autowired
+    private ObjectMapper objectMapper; // Instantiating a new field variable of type ObjectMapper with the @Autowired
+                                       // annotation.
+
+    @Test
+    public void testGetAllHighscores() throws Exception {
+
+        List<UserScore> highscores = objectMapper.readValue(new FileInputStream("./../appdata/highscore.json"),
+                new TypeReference<List<UserScore>>() {
+                }); // Read in the userScores from the highscore.json file.
+
+        mockMvc.perform(get("/highscores")) // Perform a GET request to the /highscores endpoint.
+                .andExpect(status().isOk()) // Expect the status code to be 200 (OK).
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)) // Expect the content type to be JSON.
+                // Check that the response in JSON format is equal to the highscores list
+                // written as a JSON string.
+                .andExpect(content().json(objectMapper.writeValueAsString(highscores)));
+    }
+
+    @Test
+    public void addHighscore() throws Exception {
+        UserScore oskar = new UserScore("oskar", 15, "2023-10-15", "EASY");
+
+        mockMvc.perform(post("/highscores") // Perform a POST request to the /highscores endpoint.
+                .contentType(MediaType.APPLICATION_JSON) // Set the content type to JSON.
+                .content(objectMapper.writeValueAsString(oskar))) // Set the content of the request to the oskar
+                                                                  // UserScore object written as a JSON string.
+                .andExpect(status().isOk()); // Expect the status code to be 200 (OK).
+
+        List<UserScore> highscores = objectMapper.readValue(new FileInputStream("./../appdata/highscore.json"),
+                new TypeReference<List<UserScore>>() {
+                }); // Read in the userScores from the highscore.json file.
+
+        // Check that the oskar UserScore object is in the highscores list.
+        assertTrue(highscores.stream()
+                .anyMatch(userScore -> userScore.getName().equals(oskar.getName()) && userScore.getScore() == 15
+                        && userScore.getDate() == "2023-10-15" && userScore.getDifficulty() == "EASY"));
+
+        // Delete oskar from the highscore file, he is only there for tsting purposes.
+        HighscoreFileManager.deleteFromHighscore(oskar.getName(), oskar.getScore(), oskar.getDate());
+    }
+}
