9da78bd6cec1fa296e3f8b24b35f2d9a636d01a6
Christian Fredrik <cjohnsen41@gmail.com>
2023-11-01 03:41:10 +0100

    Removed empty lines.

--- minesweeper/storage/src/main/java/springBoot/SpringApp.java
+++ minesweeper/storage/src/main/java/springBoot/SpringApp.java
@@ -3,7 +3,6 @@ package springBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SpringApp {
 
