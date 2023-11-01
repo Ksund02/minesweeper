cf72b5c2dff1526d7bfc9a24b7ce2582afc1e1fb
Christian Fredrik <cjohnsen41@gmail.com>
2023-11-01 01:45:05 +0100

    Tried to add a new test called testAddHighscore.

--- minesweeper/storage/src/test/java/storage/HighscoreRestControllerTest.java
+++ minesweeper/storage/src/test/java/storage/HighscoreRestControllerTest.java
@@ -20,30 +20,49 @@ import java.util.List;
 import springBoot.HighscoreService;
 import springBoot.SpringApp;
 
+/**
+ * This class is used to test the HighscoreRestController.
+ * It sets up a mock-version of highscoreService, and then uses this mock to test
+ * the different methods in HighscoreRestController.
+ */
 @SpringBootTest(classes = SpringApp.class)
 @AutoConfigureMockMvc
 public class HighscoreRestControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mockMvc; // This guy is used to simulate HTTP interactions. It's a mock, so it's not a real server.
 
     @MockBean
     private HighscoreService highscoreService;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private ObjectMapper objectMapper; // Instantiating a new field variable of type ObjectMapper with the @Autowired annotation.
 
     @Test
     public void testGetAllHighscores() throws Exception {
+        
         UserScore oskar = new UserScore("oskar", 15, "2023-10-15", "EASY");
         UserScore david = new UserScore("david", 20, "2023-10-08", "EASY");
         List<UserScore> highscores = Arrays.asList(oskar, david);
 
         when(highscoreService.getAllHighscores()).thenReturn(highscores);
 
-        mockMvc.perform(get("/highscores"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(objectMapper.writeValueAsString(highscores)));
-    }   
+        mockMvc.perform(get("/highscores")) // Perform a GET request to the /highscores endpoint.
+                .andExpect(status().isOk()) // Expect the status code to be 200 (OK).
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON)) // Expect the content type to be JSON.
+                .andExpect(content().json(objectMapper.writeValueAsString(highscores))); // Check that the response in JSON format is equal to the highscores list written as a JSON string.
+    }
+    
+    @Test
+    public void testAddHighscore() throws Exception {
+        UserScore oskar = new UserScore("oskar", 15, "2023-10-15", "EASY");
+
+        mockMvc.perform(post("/highscores") // Perform a POST request to the /highscores endpoint.
+                .contentType(MediaType.APPLICATION_JSON) // Specify that the content type is JSON.
+                .content(objectMapper.writeValueAsString(oskar))) // Write the UserScore object as a JSON string.
+                .andExpect(status().isOk()); // Expect the status code to be 200 (OK).
+
+        verify(highscoreService, times(1)).addHighscore(oskar); // Verify that the addHighscore method in highscoreService is called once with the oskar object as an argument.
+    
+    }
 }
