6d380a89d551f29502e52f73f435c61ef88d9a19
Christian <cfj@johnsen.no>
2023-11-04 09:14:28 +0100

    Fix package name and add comments to HighscoreRestController and HighscoreService

--- minesweeper/storage/src/main/java/springBoot/HighscoreService.java
+++ minesweeper/storage/src/main/java/springBoot/HighscoreService.java
@@ -1,22 +1,27 @@
-package springBoot;
+package springboot;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
 import storage.HighscoreFileManager;
 import storage.UserScore;
 
+/**
+ * This class is used to get appropriate responses to HTTP requests.
+ * The controller sends the requests to this class, which then
+ * returns the appropriate response.
+ */
 @Service
 public class HighscoreService {
 
-    public List<UserScore> getAllHighscores() {
-        return HighscoreFileManager.readFromHighscore(HighscoreFileManager.getFile());
-    }
+  public List<UserScore> getAllHighscores() {
+    return HighscoreFileManager.readFromHighscore(HighscoreFileManager.getFile());
+  }
 
-    public void addHighscore(UserScore userScore) {
-        HighscoreFileManager.writeToHighscore(userScore, HighscoreFileManager.getFile());
-    }
+  public void addHighscore(UserScore userScore) {
+    HighscoreFileManager.writeToHighscore(userScore, HighscoreFileManager.getFile());
+  }
 
-    public void clearAllHighscores() {
-        HighscoreFileManager.clearHighscore(HighscoreFileManager.getFile());
-    }
+  public void clearAllHighscores() {
+    HighscoreFileManager.clearHighscore(HighscoreFileManager.getFile());
+  }
 }
