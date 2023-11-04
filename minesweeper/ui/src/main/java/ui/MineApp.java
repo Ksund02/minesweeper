2f9b45f4b9d5e90c030a1015a7ce33fe89b8e967
Christian <cfj@johnsen.no>
2023-11-04 08:15:07 +0100

    MineApp: Follows checkstyle standard

--- minesweeper/ui/src/main/java/ui/MineApp.java
+++ minesweeper/ui/src/main/java/ui/MineApp.java
@@ -1,5 +1,7 @@
 package ui;
 
+import core.settings.SettingsManager;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,32 +9,28 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-import core.settings.SettingsManager;
-
 /**
- * JavaFX App
+ * JavaFX App.
  */
 public class MineApp extends Application {
-    /**
-     * Initializing the game scene, setting the icon, and starts to play music.
-     */
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/ui/GamePage.fxml"));
-        Parent parent = fxmlLoader.load();
-        stage.setScene(new Scene(parent));
+  /**
+   * Initializing the game scene, setting the icon, and starts to play music.
+   */
+  @Override
+  public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/ui/GamePage.fxml"));
+    Parent parent = fxmlLoader.load();
+    stage.setScene(new Scene(parent));
 
-        Image icon = new Image(getClass().getResourceAsStream("/images/truls.jpg"));
-        stage.getIcons().add(icon);
+    Image icon = new Image(getClass().getResourceAsStream("/images/truls.jpg"));
+    stage.getIcons().add(icon);
 
-        stage.setWidth(SettingsManager.getGameDifficulty().getStageMinWidth()+1);
-        stage.setHeight(SettingsManager.getGameDifficulty().getStageMinHeight()+1);
-        stage.show();
-    }
+    stage.setWidth(SettingsManager.getGameDifficulty().getStageMinWidth() + 1);
+    stage.setHeight(SettingsManager.getGameDifficulty().getStageMinHeight() + 1);
+    stage.show();
+  }
 
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main(String[] args) {
+    launch();
+  }
 }
